06b02bd178a676fa1c25bb57e4f3b4dfc57b05ca
joaopedro <jpssantosos5874@gmail.com>
2026-02-08 19:09:45 -0300

    Added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 
 @Component({
@@ -8,5 +8,5 @@ import {HeaderComponent} from './header/header.component';
   styleUrl: './app.component.css'
 })
 export class App {
-  protected readonly title = signal('investment-calculator');
+
 }
