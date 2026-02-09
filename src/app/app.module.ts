c2e97c2e608797a51fd6ba2f35327e58a058269a
joaopedro <jpssantosos5874@gmail.com>
2026-02-08 23:26:47 -0300

    Added board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderModule} from './header/header.module';
 import {BrowserModule} from '@angular/platform-browser';
+import {BoardModule} from './board/board.module';
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderModule]
+  imports: [BrowserModule, HeaderModule, BoardModule]
 })
 export class AppModule {
 }
---
+++ src/app/board/board.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {BoardComponent} from './board.component';
+
+@NgModule({
+  declarations: [BoardComponent],
+  exports: [BoardComponent]
+})
+export class BoardModule{}
