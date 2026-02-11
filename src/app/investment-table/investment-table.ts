de6bec9c8112ba890f2ccdd57d227d403b6fe29e
joaopedro <jpssantosos5874@gmail.com>
2026-02-11 03:11:02 -0300

    Implemented SheetJS lib to export data to xlsx

--- src/app/investment-table/investment-table.ts
+++ src/app/investment-table/investment-table.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {InvestmentModel} from './invesmentModel';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-investment-table',
@@ -10,5 +11,11 @@ import {InvestmentModel} from './invesmentModel';
 export class InvestmentTable {
   @Input({required: true}) data!: InvestmentModel[]
 
+  exportToExcel() {
+    const workSheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.data);
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, workSheet, 'Sheet1');
 
+    XLSX.writeFile(workbook, 'investment-exported.xlsx');
+  }
 }
