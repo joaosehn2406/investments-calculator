9006e89dfb7e21c2efb26c414e9af0d5b7baf8c5
joao pedro <jpssantosos5874@gmail.com>
2026-02-18 21:54:09 -0300

    Implemented item selection in modal

--- frontend/src/app/features/board/modal/modal.component.ts
+++ frontend/src/app/features/board/modal/modal.component.ts
@@ -1,7 +1,8 @@
-import {Component, input, output, signal} from '@angular/core';
+import {Component, inject, input, output, signal} from '@angular/core';
 import {LocalStorageModel} from '../../../shared/model/localStorage.model';
 import {SearchFilterPipe} from './search-filter.pipe';
 import {DatePipe} from '@angular/common';
+import {ToastService} from '../../../core/services/toast.service';
 
 @Component({
   selector: 'app-modal',
@@ -18,8 +19,29 @@ export class ModalComponent {
   closeModal = output<void>()
 
   searchTerm = signal('')
+  selectedIds = signal<Set<string>>(new Set<string>())
+
+  private toastService = inject(ToastService);
 
   onCloseModal() {
     this.closeModal.emit()
+  }
+
+  onClickSelectItem(item: LocalStorageModel) {
+    const current = this.selectedIds();
+
+    if(current.has(item.id)) {
+      current.delete(item.id);
+    } else if (current.size < 2) {
+      current.add(item.id);
+    } else {
+      this.toastService.show("Reached limit of 2 items!", 'error');
+    }
+
+    this.selectedIds.set(new Set<string>(current))
+  }
+
+  validateInvestmentType() {
+
   }
 }
