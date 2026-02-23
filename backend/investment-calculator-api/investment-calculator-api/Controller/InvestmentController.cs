ddb955b1f1cd8f4bca3f4443ef51cecb69ec579d
joao pedro <jpssantosos5874@gmail.com>
2026-02-23 19:22:47 -0300

    Created api controller

---
+++ backend/investment-calculator-api/investment-calculator-api/Controller/InvestmentController.cs
@@ -0,0 +1,22 @@
+﻿using investment_calculator_api.DTO;
+using investment_calculator_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace investment_calculator_api.Controller;
+
+[ApiController]
+[Route("api/investments")]
+public class InvestmentController : ControllerBase
+{
+  private readonly InvestmentService _investmentService;
+
+  public InvestmentController(InvestmentService investmentService)
+    => _investmentService = investmentService;
+
+  [HttpPost("calculate")]
+  public IActionResult Calculate([FromBody] CalculationRequest request)
+  {
+    var response = _investmentService.Calculate(request);
+    return Ok(response);
+  }
+}
