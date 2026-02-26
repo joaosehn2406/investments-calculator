12640e70f1c7ccd1694610fe33192c104afa30c6
joao pedro <jpssantosos5874@gmail.com>
2026-02-26 00:35:49 -0300

    Created models for DB connection

---
+++ backend/investment-calculator-api/investment-calculator-api/Model/Investment.cs
@@ -0,0 +1,13 @@
+﻿namespace investment_calculator_api.Model;
+
+public class Investment
+{
+  public Guid Id { get; set; }
+  public string Title { get; set; }
+  public string Description { get; set; }
+  public string Currency { get; set; }
+  public string InvestmentType { get; set; }
+  public DateTime CalculatedAt { get; set; }
+
+  public List<InvestmentResult> Results { get; set; } = [];
+}
---
+++ backend/investment-calculator-api/investment-calculator-api/Model/InvestmentResult.cs
@@ -0,0 +1,14 @@
+﻿namespace investment_calculator_api.Model;
+
+public class InvestmentResult
+{
+  public Guid Id { get; set; }
+  public Guid InvestmentId { get; set; }
+  public int Period { get; set; }
+  public decimal InvestmentValue { get; set; }
+  public decimal InterestYear { get; set; }
+  public decimal TotalInterest { get; set; }
+  public decimal InvestedCapital { get; set; }
+
+  public Investment Investment { get; set; } = null!;
+}
